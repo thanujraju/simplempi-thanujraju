0333dd8e3e93f75d046c48c6fdb1e18e7ca12b54
thanujraju <p.thanuj97@gmail.com>
2018-04-02 22:07:53 +0530

    stg1 in progress

--- student.cpp
+++ student.cpp
@@ -4,6 +4,19 @@ MPI_Datatype get_custom_type(int num_enemy) {
      /*
       *      Your Code Goes Here
       */
+     /////////////
+     MPI_Datatype type[8] = {MPI_INT,  ,MPI_CHAR,MPI_DOUBLE,MPI_DOUBLE,MPI_INT,MPI_DOUBLE,MPI_INT};
      
      return retValue;
 }
+
+struct enemy {
+  int count;                    // num of units 1 to 20
+  int location[20][3];          // location of units x,y,z
+  char type[20];                // one of 'a,b,c,d,e'
+  double experience;            // experience of enemy 0-20 yrs
+  double strength;              // 0-100, 0 -> dead, 100-> full
+  int supplies;                 // num of days
+  double fuel;                  // obvious
+  int stance;                   // 0-10, defensive to offensive
+};
